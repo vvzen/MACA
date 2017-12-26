120a18df05aad7e6b5b338f0bb50c6593c320951
Valerio Viperino <valerio.viperino@gmail.com>
2017-12-26 17:11:53 +0100

    updated lab code

--- week-5/wcc/lab/template/ofApp.cpp
+++ week-5/wcc/lab/template/ofApp.cpp
@@ -3,69 +3,51 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofBackground(0);
-    font.load("arial", 100, false, false, true);
+    font.load("arial", 100, false, false, true);  //function to load the font
     sentence = "Goldsmiths";
-    string_points = getStringPoints(sentence, 100); // stringPoints contains now the points that make up the word "Goldsmiths"
+    stringPoints = getStringPoints(sentence, 100);   //stringPoints contains now the points that make up the word "Goldsmiths"
 
-    for (int i = 0; i < string_points.size(); i++){
+    // YOUR CODE STARTS HERE
 
-        noise_seeds.push_back(ofRandom(0, 1000));
-    }
+
+    // YOUR CODE ENDS HERE
+
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 
+    // YOUR CODE STARTS HERE
+
+
+    // YOUR CODE ENDS HERE
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     ofPushMatrix();
-    ofTranslate((ofGetWidth() - font.stringWidth(sentence)) / 2, 250);
+    ofTranslate((ofGetWidth() - font.stringWidth(sentence))/2, 250);
 
-    // ofBeginShape();
-    // ofNoFill();
-    // ofSetPolyMode(OF_POLY_WINDING_NONZERO);
+    // YOU CODE STARTS HERE
 
-    float noise_size = ofMap(mouseX, 0, ofGetWidth(), 0, 100);
 
-    for (int i = 0; i < string_points.size(); i++){
+    // END OF YOUR CODE
 
-        float offset_x  = ofSignedNoise(noise_seeds[i]) * noise_size;
-        float offset_y  = ofSignedNoise(noise_seeds[i] + 55) * noise_size;
-
-        float point_size = ofSignedNoise(noise_seeds[i]) * 6;
-
-        ofColor col_1 = ofColor(234, 225, 0);
-        ofColor col_2 = ofColor(193, 37, 0);
-        ofColor color = col_1.getLerped(col_2, ofMap(point_size, -6, 6, 0, 1));
-        
-        ofSetColor(color);
-        ofPoint offset = ofPoint(offset_x, offset_y);
-        ofPoint new_pos = string_points[i] + offset;
-
-        ofVertex(new_pos);
-        ofDrawCircle(new_pos.x, new_pos.y, point_size, point_size);
-
-        // increment noise seed
-        noise_seeds[i] += 0.01;
-    }
-    // ofEndShape();
     ofPopMatrix();
 }
 
 //--------------------------------------------------------------
 vector <ofPoint> ofApp::getStringPoints(string s, int numOfSamples){
-
     vector <ofPoint> stringPoints;
-    vector <ofTTFCharacter> paths = font.getStringAsPoints(s);
+    // get the string as paths
+    vector < ofTTFCharacter > paths = font.getStringAsPoints(s);
 
-    // find the biggest character in terms of perimeter (used for uniform resampling)
+    //find the biggest character in terms of perimeter (used for uniform resampling)
     int maxPerimeter = 0;
     for (int i = 0; i < paths.size(); i++){
        vector <ofPolyline> polylines = paths[i].getOutline();
        for (int j = 0; j < polylines.size(); j++){
-           if (polylines[j].getPerimeter() > maxPerimeter) maxPerimeter = polylines[j].getPerimeter();
+           if (polylines[j].getPerimeter()>maxPerimeter) maxPerimeter = polylines[j].getPerimeter();
        }
     }
     // for every character, get its path
@@ -134,6 +116,6 @@ void ofApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
