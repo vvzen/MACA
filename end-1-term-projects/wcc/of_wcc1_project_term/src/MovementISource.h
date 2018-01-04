04ea2743f8ca899f9621b18aa2331c49c6be699f
Valerio Viperino <valerio.viperino@gmail.com>
2018-01-04 19:05:18 +0100

    still working, 1 and a half minute

--- end-1-term-projects/wcc/of_wcc1_project_term/src/MovementISource.h
+++ end-1-term-projects/wcc/of_wcc1_project_term/src/MovementISource.h
@@ -23,25 +23,35 @@ class MovementISource : public ofx::piMapper::FboSource {
         void drawMovingLines(float currentShowTime);
         // 3
         void drawMovingCircles(float currentShowTime);
+        // 4
+        void drawColouredLines(float currentShowTime);
         // events
         void onKeyPressed(ofKeyEventArgs & event);
+
+        // initialisation
+        void init_vars();
 
         // timing
         float show_start_time;
         float current_show_time;
-        bool CHECKPOINT_1 = false;
+        bool CHECKPOINT_1 = true;
         // determines the timing of the intro rectangles
         int intro_time_multiplier;
-        bool CHECKPOINT_2 = false;
+        bool CHECKPOINT_2 = true;
         float lines_start_time;
         bool lines_started = false;
         float rects_start_time;
         bool rects_started = false;
-        bool CHECKPOINT_3 = false;
+        bool CHECKPOINT_3 = true;
         float bars_start_time;
         bool bars_started = false;
         bool show_left_ellipse = false;
         bool ball_disappeared = false;
+        bool CHECKPOINT_4 = false;
+        float coloured_lines_start_time;
+        bool coloured_lines_started = false;
+        float white_bg_fade_in_time;
+        bool bg_started_fade = false;
 
         // checkpoint 3
         ofVec2f center_rect_size;
@@ -50,7 +60,13 @@ class MovementISource : public ofx::piMapper::FboSource {
         ofVec2f ellipse_velocity;
         ofVec2f ellipse_acceleration;
         float circles_size_multiplier;
-        int bounce_count;
+        unsigned int bounce_count;
+
+        // checkpoint 4
+        int num_rects_h_1;
+        int num_rects_h_2;
+        int num_rects_h_3;
+        int num_rects_h_4;
 
         // used to keep track of the timing of the rectangles
         bool rectangle_triggers[6] = {false, false, false, false, false, false};
