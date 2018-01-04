5ec4ede3722b185ab3798e3679e817f85a0c13c3
Valerio Viperino <valerio.viperino@gmail.com>
2018-01-04 13:35:17 +0100

    refactoring code

--- end-1-term-projects/pfa1/idea-1/generative_bot_stx_pfa1/src/Type.cpp
+++ end-1-term-projects/pfa1/idea-1/generative_bot_stx_pfa1/src/Type.cpp
@@ -2,23 +2,35 @@
 #include "Type.h"
 
 //--------------------------------------------------------------
-vector<Word> generateTypography(ofxJSONElement jsonSource, ofTrueTypeFont & font, float verticalSize){
+vector<Word> generate_typography(ofxJSONElement jsonSource, ofTrueTypeFont & font, float verticalSize, bool vertical){
 
-    string first_word = jsonSource[0]["word"].asString();
     vector <Word> words;
 
     int index = 0;
+    // use first word to comput the first horizontal offset
+    string first_word = jsonSource[index]["word"].asString();
     int x_offset = font.getStringBoundingBox(first_word, 0, 0).getWidth();
 
-    for (int y = 0; y < ofGetHeight(); y+=verticalSize){
-        for (int x = 0; x < ofGetWidth(); x+=x_offset){
+    int outer_limit;
+    int inner_limit;
+    if (vertical){
+        outer_limit = ofGetWidth();
+        inner_limit = ofGetHeight();
+    }
+    else {
+        outer_limit = ofGetHeight();
+        inner_limit = ofGetWidth();
+    }
+    // add all the words to the returned vector
+    for (int y = 0; y < outer_limit; y+=verticalSize){
+        for (int x = 0; x < inner_limit; x+=x_offset){
 
             int frequency = jsonSource[index]["score"].asInt();
 
+            // pick only most frequent words
             if (frequency > 2){
 
                 Word current_word;
-                // current_word.text = ofRandom(0, 2) == 1 ? "asd" : "asdfg";
 
                 current_word.text = jsonSource[index]["word"].asString();
                 // int index = x + y * (ofGetWidth() / word_bb.getWidth());
@@ -31,9 +43,6 @@ vector<Word> generateTypography(ofxJSONElement jsonSource, ofTrueTypeFont & font
                 words.push_back(current_word);
                 
                 x_offset = word_bb.getWidth();
-
-                // cout << "word: " << current_word.text << ", offset: " << x_offset << endl;
-                // verticalSize = word_bb.getHeight() * ofMap(frequency, 2, 315, 0.2, 1);
                 
                 // TODO: compute vertical offset
                 // knowing that it must correspond to the vertical size
@@ -47,59 +56,53 @@ vector<Word> generateTypography(ofxJSONElement jsonSource, ofTrueTypeFont & font
 }
 
 //--------------------------------------------------------------
-void renderTypography(ofFbo & fbo, vector<Word> words, ofTrueTypeFont & font){
+void render_typography(ofFbo & fbo, vector<Word> words, ofTrueTypeFont & font, bool vertical){
 
     fbo.begin();
 
     ofPushStyle();
     ofClear(0);
-    // ofBackground(0);
-    // ofBackground(29, 17, 77);
+    // if requested, put the artwork in portrait mode
+    if (vertical){
+        ofTranslate(fbo.getWidth()/2, fbo.getHeight()/2);
+        ofRotateZ(90);
+        ofTranslate(-fbo.getHeight()/2, -fbo.getWidth()/2, 0);
+    }
 
+    // render all the words in the vector
     for (int i = 0; i < words.size(); i++){
         
         ofPushMatrix();
-        float scale_factor = ofMap(words[i].frequency, 2, 315, 0.4, 1);
 
         ofTranslate(words[i].pos.x, words[i].pos.y);
-        // ofScale(scale_factor, scale_factor, 1);
+        // float scale_factor = ofMap(words[i].frequency, 2, 315, 0.4, 1);
+        // float scale_factor = ofMap(ofNoise(words[i].frequency), 0, 1, 0.4, 1);
+        float scale_factor = ofRandom(0.4, 1);
+        ofScale(scale_factor, scale_factor, 1);
         
         // ofDrawRectangle(0, 0, words[i].size.x, words[i].size.y);
         
         ofPushMatrix();
         ofTranslate(0, words[i].size.y, 0);
 
-        // ofColor color;
-        // color.r = ofMap(i, 0, words.size(), 255, 0);
-        // color.g = ofMap(i, 0, words.size(), 255, 0);
-        // color.b = ofMap(i, 0, words.size(), 255, 0);
-
-        ofSetColor(255);
-
-        if (words[i].frequency % 2 == 0){
-            font.drawString(words[i].text, 0, 0);
+        // throw a d6, with 1 we hide contours of the current word
+        if (int(ofRandom(6)) == 1){
+            ofNoFill();
+            font.drawStringAsShapes(words[i].text, 0, 0);
         }
         else {
+            ofFill();
             font.drawString(words[i].text, 0, 0);
-            // font_demi_bold.drawStringAsShapes(words[i].text, 0, 0);
         }
+
         ofPopMatrix();
         
+        // use the draw rectangle to debug the position of the words
         // ofDrawRectangle(0, 0, words[i].size.x, words[i].size.y);
-
-        // ofDrawCircle(0, 0, 5, 5);
-        
-        // if (scale_factor > 0.5){
-        //     font_demi_bold.drawString(words[i].text, 0, words[i].pos.y);
-        // }
-        // else {
-        //     font.drawString(words[i].text, 0, words[i].pos.y);
-        // }
 
         ofPopMatrix();
     }
 
-    // ofDisableBlendMode();
     ofPopStyle();
 
     fbo.end();
--- end-1-term-projects/pfa1/idea-1/generative_bot_stx_pfa1/src/Type.h
+++ end-1-term-projects/pfa1/idea-1/generative_bot_stx_pfa1/src/Type.h
@@ -2,13 +2,23 @@
 //  Type.h
 //  generative_bot_stx_pfa1
 //
-//  Created by Valerio Viperino on 31/12/17.
+//  Created by Valerio Viperino
 //
 //
+#pragma once // include this file only once
 
 #include "ofMain.h"
 
+// we don't need a full class, so we're
+// just using a struct to properties of each word
+struct Word {
+    ofVec2f pos;
+    ofVec2f size;
+	int frequency;
+    string text;
+};
+
 // main function to generate the words positions 
-vector<Word> generateTypography(ofxJSONElement jsonSource, ofTrueTypeFont & font, float verticalSize);
+vector<Word> generate_typography(ofxJSONElement jsonSource, ofTrueTypeFont & font, float verticalSize, bool vertical=false);
 // draw the generative type on the given fbo
-void renderTypography(ofFbo & fbo, vector<Word> words, ofTrueTypeFont & font);
+void render_typography(ofFbo & fbo, vector<Word> words, ofTrueTypeFont & font, bool vertical=false);
