014a618c20efad1322369a2d25aa1c326fac4be7
Valerio Viperino <valerio.viperino@gmail.com>
2018-01-07 14:28:11 +0000

    better comments

--- end-1-term-projects/pfa1/idea-1/generative_bot_stx_pfa1/src/main.cpp
+++ end-1-term-projects/pfa1/idea-1/generative_bot_stx_pfa1/src/main.cpp
@@ -12,13 +12,19 @@
 // 2.1 The boids have a small desire to flow through the Flow Field
 // 2.1.1 Desired = steering - velocity
 // 2.2 The boids have flocking and steering behaviours
-// 2.3 Each boid leaves a coloured trail
+// 2.3 Each boid leaves a white trail
 // 2.3.1 All of the paint trails should be in a separate fbo
+// PHASE 3
+// 3. Create a typographic image full of words (white on black)
+// 3.1 Compute the width and height of each word in order to fill up the space without overlapping words
+// 3.2 Render this image in a separate fbo
+// 3.3 The boids have fear of these words (white pixels) and will avoid them
+// Accidentally, they will keep bouncing inside the letters before finding a way out
 
 //========================================================================
 int main(int argc, char * argv[]){
-	// ofSetupOpenGL(2560,1440,OF_FULLSCREEN);			// <-------- setup the GL context
-	ofSetupOpenGL(2304,1296,OF_WINDOW);			// <-------- setup the GL context
+	// ofSetupOpenGL(2304,1296,OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
     ofApp * myApp = new ofApp();
     
