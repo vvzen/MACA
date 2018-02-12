32c599e9852f654e8d88cc0f66161c1725e4206e
Valerio Viperino <vv@pc-139-10.customer.greenwich.grosvenor.lan>
2018-02-12 15:32:09 +0000

    updated script to generate subfolders

--- generate-folders.py
+++ generate-folders.py
@@ -1,6 +1,6 @@
 import os
 
-NUMBER_OF_WEEKS = 19
+NUMBER_OF_WEEKS = 22
 
 current_week = 1
 target_path = os.path.dirname(os.path.abspath(__file__))
@@ -8,9 +8,16 @@ target_path = os.path.dirname(os.path.abspath(__file__))
 while current_week <= NUMBER_OF_WEEKS:
     dir_name = "week-{}".format(str(current_week))
     dir_path = os.path.join(target_path, dir_name)
+
+    # also generate the subfolders
+    folders = [dir_path, os.path.join(dir_path, "wcc"), os.path.join(dir_path, "pfa"), os.path.join(dir_path, "phc"), os.path.join(dir_path, "assignments")]
+
     # print "dir_name: {}".format(dir_name)
-    try:
-        os.mkdir(dir_path)
-    except OSError:
-        pass
+    for sf in folders:
+        try:
+            print sf
+            os.mkdir(sf)
+        except OSError:
+            continue
+    
     current_week += 1
